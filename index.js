8e598c55e7a6d733783f9dae89bb9325839e9304
b0939261761 <b0939261761@gmail.com>
2019-11-01 18:23:04 +0200

    change sort  proxies

--- index.js
+++ index.js
@@ -12,28 +12,22 @@ const parse = require('./services/parse');
 
 // -----------------------------
 
-const getProxy = async ({ proxies, currentProxyIndex }) => {
-  let [list, currentIndex] = [proxies, currentProxyIndex];
+const getProxy = async () => {
+  const proxy = await db.getProxy();
+  if (!proxy) throw new Error('NO_PROXY');
 
-  if (currentIndex >= list.length) {
-    list = await db.getProxies();
-    currentIndex = 0;
-    if (!list.length) throw new Error('NO_PROXY');
-  }
-
-  const proxy = list[currentIndex];
   const timeout = process.env.REPEAT_PROXY_TIMEOUT * 1000 - Date.now() + proxy.lastActive;
   if (timeout > 0) await delay(timeout);
 
-  return { proxies: list, currentProxyIndex: currentIndex, proxy };
+  return proxy;
 };
 
 // ------------------------
 
 (async () => {
-  for (let currentProxyIndex = 0, proxies = [], proxy; ;++currentProxyIndex) {
+  for (;;) {
     try {
-      ({ proxies, currentProxyIndex, proxy } = await getProxy({ proxies, currentProxyIndex }));
+      const proxy = await getProxy();
       if ((await getCaptchaBalance()) < 1) throw new Error('CAPTCHA_NO_BALANCE');
       const code = nextCode((await db.getLastCode()) || '40200240');
 
